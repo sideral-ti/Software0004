a2ba43f3bd173c9f5d32ff117144a4b6b0065b0c
Sideral TI <sideralti.appgmail.com>
2024-10-27 22:37:59 -0300

    initial project

---
+++ src/main/java/cl/sideralti/app/Software0004Application.java
@@ -0,0 +1,13 @@
+package cl.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Software0004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Software0004Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sideralti/app/Software0004ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sideralti.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Software0004ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
